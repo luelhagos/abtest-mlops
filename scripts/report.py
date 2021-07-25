660c7ab07a33b3a142277618a625f2b7cea90fe8
Luel Hagos <luelh0101@gmail.com>
2021-07-25 23:53:32 +0300

    update

--- scripts/report.py
+++ scripts/report.py
@@ -1,11 +1,11 @@
 with open('results.txt', 'w') as result:
       result.write('Logistic Regression\n')
-      result.write(f'score : 0.518, and loss: 0.701\n')
+      result.write(f'score : 0.518. \nloss: 0.701\n')
 
       result.write('\n')
       result.write('Decision Tree\n')
-      result.write(f'score : 0.839, and loss: 12.04  \n')
+      result.write(f'score : 0.839. \nloss: 12.04  \n')
 
       result.write('\n')
       result.write('XGBoost\n')
-      result.write(f'score : 0.518, and loss: 0.712\n')
+      result.write(f'score : 0.518. \nloss: 0.712\n')
